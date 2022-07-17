d4da21282145199e5aa67b9cf07c6c0478901394
nomad-dev <73686008+david-chemwetich@users.noreply.github.com>
2022-07-17 17:40:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@
 #  ░   ▒    ▒ ░   ░ ░   ░   ░░   ░ ░ ░ ░ ▒   ░░░ ░ ░ ░░            ░   ▒   ░  ░  ░  ░  ░  ░   ▒ ░░ ░   ░    ░   ░ ░ ░      ░      ░      ░   ░ ░   ░      
 #      ░  ░ ░           ░    ░         ░ ░     ░                       ░  ░      ░        ░   ░        ░          ░        ░      ░  ░         ░          
 #
-# Group Assignment AI Stock Market Prediction -> https://github.com/david-chemwetich 
+# Group Assignment AI Stock Market Prediction -> https://github.com/david-chemwetich/Stock-Market-Prediction-with-Python
 # The project was compiled to use  AVX 
 # incase you get this error use colab -> "The TensorFlow library was compiled to use AVX instructions, but these aren't available on your machine."
 #https://colab.research.google.com/drive/1qNlOva3TUtzhjLDaArXZ_qedLfVL0WM3?usp=sharing
